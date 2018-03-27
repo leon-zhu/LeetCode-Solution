2474cd405799b47e83d2982ba684fb9c35c79e88
Sunny_sunny321 <lyzhu0318@163.com>
2018-03-27 14:50:40 +0800

    solution for title 206

---
+++ 206-Reverse Linked List.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ //题目: 反转单链表
+ //思路: 定义两个指针，一个指向左侧反转后的链表head，另一个指向右侧未反转的链表head
+ //注意:
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+    }
+}
